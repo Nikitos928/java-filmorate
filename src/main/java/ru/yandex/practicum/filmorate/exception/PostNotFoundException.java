afc47de762cd1d430b4f209c1258f32f7a5eac08
Nikitos928 <110191390+Nikitos928@users.noreply.github.com>
2023-02-05 21:00:51 +0300

    Delete src/main/java/ru/yandex/practicum/filmorate/exception directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
