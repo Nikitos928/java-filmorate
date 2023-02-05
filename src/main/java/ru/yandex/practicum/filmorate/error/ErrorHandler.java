10135b69549739b37fcceed5bd38a52b46a015b6
Nikitos928 <110191390+Nikitos928@users.noreply.github.com>
2023-02-05 16:30:17 +0200

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/error/ErrorHandler.java
@@ -0,0 +1,46 @@
+package ru.yandex.practicum.filmorate.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.*;
+
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidEmailException(final InvalidEmailException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlePostNotFoundException(final PostNotFoundException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleUserNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handlePostNotFoundException(final UserAlreadyExistException e) {
+        return new ErrorResponse(
+                e.getMessage()
+        );
+    }
+
+}
