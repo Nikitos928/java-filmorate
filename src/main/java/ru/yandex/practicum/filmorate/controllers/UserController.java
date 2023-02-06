c98807c568dbc777f116dfc5d7492dbc79e5e123
Nikitos928 <110191390+Nikitos928@users.noreply.github.com>
2023-02-06 19:16:53 +0200

    Add files via upload

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,61 +1,63 @@
 package ru.yandex.practicum.filmorate.controllers;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+
 public class UserController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final Map<Integer, User> users = new HashMap<>();
-    private int generatedId = 1;
+    private final UserService userService;
 
-    @PostMapping(value = "/users")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public User addUser(@Valid @RequestBody User user) throws ValidationException {
-        log.info("Получен запрос: add-user");
-        checkWhitespace(user);
-        if (user.getName() == null || user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
-        user.setId(generatedId);
-        users.put(generatedId, user);
-        generatedId++;
-        return user;
+        return userService.addUser(user);
     }
 
-    @PutMapping(value = "/users")
+    @PutMapping
     public User updateUser(@Valid @RequestBody User user) throws ValidationException {
-        log.info("Получен запрос: update-user");
-        checkWhitespace(user);
-        if (user.getName() == null || user.getName().isEmpty()) {
-            user.setName(user.getLogin());
-        }
-        if (users.containsKey(user.getId())) {
-            users.put(user.getId(), user);
-            return user;
-        } else {
-            throw new ValidationException("Пользователя с таким id нет");
-        }
+        return userService.updateUser(user);
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getUsers() {
-        return new ArrayList<>(users.values());
+        return userService.getUsers();
     }
 
-    private void checkWhitespace(User user) throws ValidationException {
-        if (user.getLogin().contains(" ")) {
-            log.info("Login содержит пробел");
-            throw new ValidationException("Name содержит пробел");
-        }
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable(value = "id") Long userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{id}/friends/{friendId}")
+    public User addFriend(@PathVariable(value = "id") Long userId, @PathVariable(value = "friendId") Long friendId) {
+
+        return userService.addFriend(userId, friendId);
+    }
+
+    @DeleteMapping("/{id}/friends/{friendId}")
+    public User deleteFriend(@PathVariable(value = "id") Long userId, @PathVariable(value = "friendId") Long friendId) {
+        return userService.deleteFriend(userId, friendId);
+    }
+
+    @GetMapping("/{id}/friends")
+    public List<User> getFriends(@PathVariable(value = "id") Long userId) {
+        return userService.getFriends(userId);
+    }
+
+    @GetMapping("/{id}/friends/common/{otherId}")
+    public List<User> mutualFriends(@PathVariable(value = "id") Long userId, @PathVariable(value = "otherId") Long otherId) {
+        return userService.mutualFriends(userId, otherId);
     }
 
 
