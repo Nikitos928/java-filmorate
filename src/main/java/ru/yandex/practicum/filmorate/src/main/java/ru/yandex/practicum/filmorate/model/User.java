869ab484d2adae5fa265bbc2d53456db4eb35f52
Nikitos928 <110191390+Nikitos928@users.noreply.github.com>
2023-02-06 19:10:21 +0200

    Add files via upload

---
+++ src/main/java/ru/yandex/practicum/filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,31 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private Long id;
+    @Email
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    private String login;
+    private String name;
+    @PastOrPresent
+    private LocalDate birthday;
+    private Set<Long> friendIds = new HashSet<>();
+
+}
